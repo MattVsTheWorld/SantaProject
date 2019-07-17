8dea1288a992468814b868fafbdb45c7ea702565
Matteo <matteo.marcuzzo.it@gmail.com>
2019-07-17 21:20:54 +0200

    CCS style

--- src/main/main.go
+++ src/main/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"time"
 )
 
@@ -29,13 +28,42 @@ type Sem struct{
 
 func (self *Sem) Run() {
 	for {
+		// aspetta il segnale su wait, lo butta
 		<- self.Wait
 		fmt.Println("Mutex semaphore put on wait")
 		<- self.Signal
-		fmt.Printf("Mutex sempahore signaled open")
+		fmt.Println("Mutex sempahore signaled open")
 	}
 }
 
+type DeerCounter struct{
+	deerNum 		int
+	Return 			chan Signal
+	Reset 			chan Signal
+	CheckDeers 		chan Signal
+	PrepareSleigh 	chan Signal
+	//santaSignal chan Signal
+}
+
+func (self *DeerCounter) Run() {
+	var curVal int
+	for {
+		curVal = self.deerNum
+		if curVal == 9 {
+			// TODO: signal Santa
+			self.PrepareSleigh <- Signal{}
+			curVal = 0	// reset counter
+
+		} else if curVal < 9 {
+			select {
+				case self.Return <- Signal{}: 	// TODO: In realta' sara' una renna a dirlo
+					curVal = curVal + 1
+				case self.CheckDeers <- Signal{}:
+		}
+
+		}
+	}
+}
 
 /* SANTA
 santaSem . wait ()
@@ -49,10 +77,22 @@ mutex . wait ()
 mutex . signal ()
 */
 
-func santa (santSem *SantaSem, mutexSem *Sem){
+func santa (santSem *SantaSem, mutexSem *Sem, deerCount *DeerCounter){
 	for {
+		// invia il segnale su wait
 		santSem.Wait <- Signal{}
 		mutexSem.Wait <- Signal{}
+		time.Sleep()
+		fmt.Println("Santa is awake, choosing who to help...")
+		select {
+			//SantaDeers
+			case deerCount.PrepareSleigh <- Signal{}:
+				fmt.Printf("There are 9 deer, C H R I S T M A S T I M E\n")
+			// checkDeers.SantaElves
+			case deerCount.CheckDeers <- Signal{}:
+				fmt.Printf("Deer are not 9; help elves\n")
+		}
+
 	}
 }
 
@@ -63,9 +103,11 @@ func main(){
 	go ss.Run()
 	ms := Sem{ make(chan Signal), make(chan Signal)}
 	go ms.Run()
-	go santa(&ss, &ms)
+	dc := DeerCounter{9, make(chan Signal), make(chan Signal),
+	                make(chan Signal), make(chan Signal)}
+	go santa(&ss, &ms, &dc)
 
-	time.Sleep(time.Duration(rand.Int63n(2*1e9)))
+	time.Sleep(time.Duration(5*time.Second))
 }
 
 
